2516fad41ec7801a48536fa16c7a76ecef038d2f
unknown <hamid.dmvtek.com>
2025-08-20 17:24:54 -0400

    code fix

--- src/Haleem/javaclass.java
+++ src/Haleem/javaclass.java
@@ -4,12 +4,6 @@ public class javaclass {
 
     public static void main(String[] args) {
 //asdfasdfsad
-        fasdfasd
-                f
-                asdf
-
-                        safd
-                        a
-                                sfd
+   
     }
 }
