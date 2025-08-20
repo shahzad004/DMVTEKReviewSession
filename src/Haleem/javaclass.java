21723a1e4ea3ed260614f375f75bf3bec117080c
unknown <hamid.dmvtek.com>
2025-08-20 17:17:31 -0400

    asdfasdf

--- src/Haleem/javaclass.java
+++ src/Haleem/javaclass.java
@@ -3,6 +3,13 @@ package Haleem;
 public class javaclass {
 
     public static void main(String[] args) {
+//asdfasdfsad
+        fasdfasd
+                f
+                asdf
 
+                        safd
+                        a
+                                sfd
     }
 }
