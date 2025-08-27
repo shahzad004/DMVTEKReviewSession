237c905342473c5ca63a5bcbf0fbfb40e26adc51
Shahzad Khan <75515143+shahzad004@users.noreply.github.com>
2025-08-27 14:16:23 -0700

    shahzad Code for method practice.

---
+++ src/shahzadCode/JavaMethods.java
@@ -0,0 +1,19 @@
+package shahzadCode;
+
+public class JavaMethods {
+
+
+    public static double sum(int a, double b){
+        double result = a + b;
+        System.out.println(" Sum of A + B = " + result );
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        sum(20,40.6);
+        sum(90,400.2);
+
+
+    }
+}
